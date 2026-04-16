c286a0dee2314c12fc17af8745dd8e2fcf4be370
CesarAbraham0428 <lopezcesar0428@gmail.com>
2026-04-15 19:04:05 -0600

    Cambios

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'una_clave_secreta_muy_dificil_de_adivinar'
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
